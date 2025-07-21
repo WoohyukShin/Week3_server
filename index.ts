5f691cf84814c962b4e2ca07a9ffcf336e4c586e
whshin <urihiya1@kaist.ac.kr>
2025-07-21 23:20:01 +0900

    TLQKF

--- index.ts
+++ index.ts
@@ -47,8 +47,13 @@ const corsOptions = {
   optionsSuccessStatus: 200
 };
 const app = express();
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 const server = http.createServer(app);
 const io = new Server(server, {
